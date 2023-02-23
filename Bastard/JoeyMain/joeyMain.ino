e35a59803f7e8d71b3db9a277f054825aafdd0a3
JoeyKraemer <joey.kramer@student.nhlstenden.com>
2023-02-23 15:55:58 +0100

    worked on line detection

--- Bastard/JoeyMain/joeyMain.ino
+++ Bastard/JoeyMain/joeyMain.ino
@@ -3,7 +3,14 @@
 #include <SoftwareSerial.h> //Bluetooth connection
 #include <QTRSensors.h> // sensor line following
 
+
 //Pins
+
+//wheels
+#define motorLeftForward 6
+#define motorLeftBackwards 5
+#define motorRightForward 3
+#define motorRightBackwards 11
 
 // sensors
 QTRSensors qtr;
@@ -19,13 +26,12 @@ void setup(){
     //sensors
     qtr.setTypeAnalog();
     qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5, A6, A7}, SensorCount);
-    //qtr.setEmitterPin(2);
-    
+    setCalibration();
 }
+bool c = true;
 //loop
 void loop(){
-    // read raw sensor values
-    qtr.read(sensorValues);
+    qtr.readLine(sensorValues);
 
     // print the sensor values as numbers from 0 to 1023, where 0 means maximum
     // reflectance and 1023 means minimum reflectance
@@ -39,4 +45,49 @@ void loop(){
     delay(250);
 }
 
+
 //Methods
+void followline(){
+    int positon = qtr.readLine(sensorValues);
+    //Finish me
+}
+
+void setCalibration(){
+   for(int i = 0; i < 2;i++){
+        forward(170);
+        for(uint8_t i = 0; i < 25 ; i++){
+            qtr.calibrate();
+            //delay(20);
+            Serial.print("Calibration: ");
+            Serial.println(i);
+        }
+        stop();
+        delay(200);
+        backward(170);
+        for(uint8_t i = 0; i < 25 ; i++){
+            qtr.calibrate();
+            //delay(20);
+            Serial.print("Calibration: ");
+            Serial.println(i);
+        }
+        Serial.println("Calibration done");
+        stop();
+    }
+}
+
+void forward(int speed){
+    analogWrite(motorLeftForward,speed);
+    analogWrite(motorRightForward,speed);
+}
+
+void stop(){
+    analogWrite(motorLeftForward,0);
+    analogWrite(motorRightForward,0);
+    analogWrite(motorLeftBackwards,0);
+    analogWrite(motorRightBackwards,0);
+}
+
+void backward(int speed){
+    analogWrite(motorLeftBackwards,speed);
+    analogWrite(motorRightBackwards,speed);
+}
