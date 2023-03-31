00abb579c5876dba2a725a462fe533a257982085
NidLip <nikolay.minins@student.nhlstenden.com>
2023-04-01 00:45:42 +0200

    final version code

--- LineSensorLogic/lineSensorLogic.ino
+++ LineSensorLogic/lineSensorLogic.ino
@@ -1,308 +1,409 @@
+//Libraries
 #include <Adafruit_NeoPixel.h>
-#include <QTRSensors.h>
+#include <QTRSensors.h> 
 
-const int PIXEL_PIN = 7;
-const int PIXEL_NUMBER = 4;
-Adafruit_NeoPixel leds(PIXEL_NUMBER, PIXEL_PIN, NEO_RGB + NEO_KHZ800);
+//Variables and pins
 
-// motor pins
-const int MOTOR_A1 = 11;
-const int MOTOR_A2 = 8;
-const int MOTOR_B1 = 9;
-const int MOTOR_B2 = 5;
-const int MOTOR_R1 = 3;
-const int MOTOR_R2 = 2;
+//NEOPIXELS
+const int PIXELS=9;
+const int PIXELS_NUMBER=4;
+Adafruit_NeoPixel pixels(PIXELS_NUMBER,PIXELS,NEO_RGB + NEO_KHZ800);
 
-// motor constants
-const int MINIMUM_SPEED = 200;
-const int MAXIMUM_SPEED = 255;
-const int KD = 5;
-const int KP = 2;
-
-// colors
-const uint32_t RED = leds.Color(255, 0, 0);
-const uint32_t YELLOW = leds.Color(234, 255, 0);
-const uint32_t BLUE = leds.Color(0, 0, 255);
-
-// line sensors
+//ULTRASENSOR
+const int TRIG_PIN=8;
+const int ECHO_PIN=7;
+int distance;
+long duration;
+//GRIPPER
+const int GRIPPER=4;
+const int GRIPPER_OPEN_PULSE=1600;
+const int GRIPPER_CLOSE_PULSE=971;
+const int GRIPPER_PULSE_REPEAT=10;
+//MOTOR
+const int MOTOR_A_FWD=5;
+const int MOTOR_A_BWD=12;
+const int MOTOR_B_FWD=6;
+const int MOTOR_B_BWD=13;
+const int MOTOR_A_R=2;
+const int MOTOR_B_R=3;
+volatile int countA;
+volatile int countB;
+const int MIN_R=130;
+const int MIN_L=140;
+//LINE SENSOR VARIABLES
 QTRSensors lineSensors;
-const int SENSOR_COUNT = 8;
+const int SENSOR_COUNT=8;
 int sensorValues[SENSOR_COUNT];
-int pos = 0;
-int error = 0;
-int lastError = 0;
+int pos=0;
 
-// button pins
-const int BUTTON1 = 12;
-const int BUTTON2 = 13;
-const int BUTTON3 = 2;
+//LINE MAZE LOGIC VARIABLES
+bool wait =true;
+bool set =false;
+bool solved=false ;
+bool victory=false;
 
-// button states;
-int buttonState1 = 0;
-int buttonState2 = 0;
-int buttonState3 = 0;
-// function declaration
+//FUNCTION HEADERS
 
-// moving functions
-void idle();                                           // keeps the motors still
-void setup_motor_pins();                               // setup the pin mode for the pins moving the DC motors
-void moveForward(int powerA = 255, int powerB = 255);  // move the robot forward
-void moveBackward(int powerA = 255, int powerB = 255); // move the robot backwards
+//MOTOR FUNCTIONS
+void setupMotorPins();
+void rotationA();
+void rotationB();
+void forward(int left=250, int right=250);
+void backward(int left=250, int right=250);
+void adjustLeft();
+void adjustRight();
+void stop();
+void rightTurn(int degree);// turns right using one motor
+void leftTurn(int degree); //turns left using one motor
+void rotateLeft(int degree);//turns left using both motors
+void leftSpin();
 
-// control functions
-void BT_control();
-void button_control();
+//ULTRA SONIC SENSORS 
+int getDistance();
+void detectObject();
 
-void setupButtons();
-
-// line sensor
+//LINE SENSOR
 void setupLineSensors();
-void calibration();
+bool middleLine();
+bool lineLeft();
+bool lineRight();
+bool allWhite();
+bool allBlack();
 bool isBlack(int value);
-void lineFollow();
 
-void setup()
+//LINE MAZE LOGIC
+void solveMaze();
+void endRace();
+void beginRace();
+
+//GRIPPER FUNCTIONS
+void gripperServo(int pulse);
+void openGripper();
+void closeGripper();
+
+
+
+
+void setup() 
 {
-    Serial.begin(9600);
+ Serial.begin (9600);
+ pixels.begin();
+ pixels.clear();
+ pixels.show();
 
-    setup_motor_pins();
-    setupButtons();
-    setupLineSensors();
-    calibration();
-    for (int i = 0; i < SENSOR_COUNT; i++)
+ setupMotorPins();
+ setupLineSensors();
+ pinMode(TRIG_PIN,OUTPUT);
+ pinMode(ECHO_PIN,INPUT);
+}
+
+void loop() 
+{
+ Serial.println("");
+ lineSensors.read(sensorValues);
+ lineSensors.calibrate();
+ pos=lineSensors.readLineBlack(sensorValues);
+ if (wait)
+ {
+  detectObject(); 
+ }
+ else if (!set)
+ {
+  beginRace();
+ }
+ else if (!solved)
+ {
+  maze();
+ }
+ else if (!victory)
+ {
+  endRace();
+ }
+}
+//FUNCTION DEFINITIONS
+
+//FUNCTION DECLARATIONS
+
+//MOTOR FUNCTIONS
+void setupMotorPins()
+{
+  pinMode(MOTOR_A_FWD,OUTPUT);
+  pinMode(MOTOR_A_BWD,OUTPUT);
+  pinMode(MOTOR_B_FWD,OUTPUT);
+  pinMode(MOTOR_B_BWD,OUTPUT);
+  pinMode(MOTOR_A_R,INPUT);
+  pinMode(MOTOR_B_R,INPUT);
+  attachInterrupt(digitalPinToInterrupt(MOTOR_A_R),rotationA,CHANGE);
+  attachInterrupt(digitalPinToInterrupt(MOTOR_B_R),rotationB,CHANGE);
+}
+
+void rotationA()
+{
+  noInterrupts();
+  countA++;
+  interrupts();
+}
+void rotationB()
+{
+  noInterrupts();
+  countB++;
+  interrupts();
+}
+void forward(int left=250,int right=250 )
+{
+  stop();
+  analogWrite(MOTOR_A_FWD,left);
+  analogWrite(MOTOR_B_FWD,right); 
+}
+void backward(int left=250,int right=250)
+{
+  stop();
+  analogWrite(MOTOR_A_BWD,left);
+  analogWrite(MOTOR_B_BWD,right);
+}
+void stop()
+{
+  analogWrite(MOTOR_A_FWD,0);
+  analogWrite(MOTOR_A_BWD,0);
+  analogWrite(MOTOR_B_FWD,0);
+  analogWrite(MOTOR_B_BWD,0);
+}
+void adjustLeft()
+{
+  stop();
+  analogWrite(MOTOR_A_FWD,130);
+  analogWrite(MOTOR_B_FWD,250);
+}
+void adjustRight()
+{
+  stop();
+  analogWrite(MOTOR_A_FWD,250);
+  analogWrite(MOTOR_B_FWD,130);
+}
+void rightTurn(int grades)
+{
+  bool rotate=true;
+  int cycles=grades;
+  countA=0;
+  while (rotate)
+  {
+    if (countA<cycles)
     {
-        Serial.print(lineSensors.calibrationOn.minimum[i]);
-        Serial.print(" ");
+      stop();
+      forward(200,0);
     }
-    Serial.println();
-    for (int i = 0; i < SENSOR_COUNT; i++)
+    else if (countA>cycles)
     {
-        Serial.print(lineSensors.calibrationOn.maximum[i]);
-        Serial.print(" ");
+      rotate=false;
     }
-    leds.begin();
-    Serial.println("Calibration done");
-    moveForward(200, 200);
+  }
 }
-
-void loop()
+void leftTurn(int grades)
 {
-
-    // button_control();
-    leds.show();
-    pos = lineSensors.readLineBlack(sensorValues);
-    for (int i = 0; i < SENSOR_COUNT; i++)
+  bool rotate=true;
+  int cycles=grades;
+  countB=0;
+  while (rotate)
+  {
+    if (countB<cycles)
     {
-        Serial.print(isBlack(sensorValues[i]));
-        Serial.print(" ");
+      stop();
+      forward(0,200);
     }
-    Serial.println();
-    lineFollow1();
+    else if (countB>cycles)
+      rotate=false;
+  }
 }
-
-void setup_motor_pins()
+void leftTurn2(int grades)
 {
-    pinMode(MOTOR_A1, OUTPUT);
-    pinMode(MOTOR_A2, OUTPUT);
-    pinMode(MOTOR_B1, OUTPUT);
-    pinMode(MOTOR_B2, OUTPUT);
-    pinMode(MOTOR_R1, INPUT);
-    pinMode(MOTOR_R2, INPUT);
-}
-
-void moveForward(int powerA = 255, int powerB = 255)
-{
-    leds.clear();
-    leds.fill(BLUE, 2, 2);
-    leds.show();
-    analogWrite(MOTOR_A2, powerA);
-    analogWrite(MOTOR_B1, powerB);
-}
-
-void moveBackward(int powerA = 255, int powerB = 255)
-{
-    leds.clear();
-    leds.fill(RED, 0, 2);
-    leds.show();
-    analogWrite(MOTOR_A1, powerA);
-    analogWrite(MOTOR_B2, powerB);
-}
-
-void idle()
-{
-    int motor_pins[4] = {MOTOR_A1, MOTOR_A2, MOTOR_B1, MOTOR_B2};
-    leds.clear();
-    for (int i = 0; i < 4; i++)
+  bool rotate=true;
+  int cycle=grades;
+  countB=0;
+  while (rotate==true)
+  {
+    if (countB < cycle)
     {
-
-        analogWrite(motor_pins[i], 0);
+      stop();
+      forward(0,150);
+      backward(150,0);
     }
+    else if (countB>cycle)
+      rotate=false;
+  }
 }
-
-void setupButtons()
+void leftSpin()
 {
-    pinMode(BUTTON1, INPUT);
-    pinMode(BUTTON2, INPUT);
-    pinMode(BUTTON3, INPUT);
+  stop();
+  analogWrite(MOTOR_B_FWD,130);
+  analogWrite(MOTOR_A_BWD,130);
 }
 
-void button_control()
+int getDistance() 
 {
-    buttonState1 = digitalRead(BUTTON1);
-    buttonState2 = digitalRead(BUTTON2);
-    if (buttonState1 == LOW)
-    {
-        moveForward(255, 255);
-    }
-    else if (buttonState2 == LOW)
-    {
-        moveBackward(255, 255);
-    }
-    else
-    {
-        idle();
-    }
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(5);
+
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  duration = pulseIn(ECHO_PIN, HIGH);
+  distance = duration * 0.034 / 2;
+
+  return distance;
 }
+
+void detectObject()
+{
+  int distance = getDistance();
+
+  if(distance < 25)
+  {
+    wait = false;
+  } 
+}
+
+//LINE SENSORS
 
 void setupLineSensors()
 {
-    lineSensors.setTypeAnalog();
-    // lineSensors.setSensorPins((const uint8_t[]){A0,A1,A2,A3,A4,A5,A6,A7},SENSOR_COUNT);
-    lineSensors.setSensorPins((const uint8_t[]){A7, A6, A5, A4, A3, A2, A1, A0}, SENSOR_COUNT);
+  lineSensors.setTypeAnalog();
+  lineSensors.setSensorPins((const uint8_t[]){A7, A6, A5, A4, A3, A2, A1, A0}, SENSOR_COUNT);
 }
-
-void calibration()
-{
-    for (int i = 0; i < 2; i++)
-    {
-        moveForward(200, 200);
-        for (uint8_t i = 0; i < 25; i++)
-        {
-            lineSensors.calibrate();
-            // delay(20);
-            Serial.print("Calibration: ");
-            Serial.println(i);
-        }
-        idle();
-        delay(100);
-        moveBackward(170, 170);
-        for (uint8_t i = 0; i < 25; i++)
-        {
-            lineSensors.calibrate();
-            // delay(20);
-            Serial.print("Calibration: ");
-            Serial.println(i);
-        }
-        Serial.println("Calibration done");
-    }
-    
-    idle();
-}
-
 bool isBlack(int value)
 {
-    return value > 700;
+  return value>700;
 }
-
-bool lineAhead()
+bool middleLine()
 {
-    if (!isBlack(sensorValues[3]) || !isBlack(sensorValues[4]))
-        return false;
-
-    for (int i = 0; i < SENSOR_COUNT; i++)
-    {
-        if (i == 3 || i == 4)
-            continue;
-        if (isBlack(sensorValues[i]))
-            return false;
-    }
-    return true;
+  return (isBlack(sensorValues[3]) && isBlack(sensorValues[4]));
 }
-
-bool lineRight()
-{
-    for (int i = SENSOR_COUNT / 2 + 1; i < SENSOR_COUNT; i++)
-    {
-        if (isBlack(sensorValues[i]))
-            return false;
-    }
-    return true;
-}
-
 bool lineLeft()
 {
-    for (int i = 0; i < SENSOR_COUNT / 2 - 1; i++)
-    {
-        if (isBlack(sensorValues[i]))
-            return false;
-    }
-    return true;
+  return (isBlack(sensorValues[0]) && isBlack(sensorValues[1]) && isBlack(sensorValues[2]));
 }
-
-bool mostlyBlack()
+bool lineRight()
 {
-    int k = 0;
-    for (int i = 0; i < SENSOR_COUNT / 2 - 1; i++)
-        if (isBlack(sensorValues[i]))
-            k = 1;
-    for (int i = SENSOR_COUNT / 2 + 1; i < SENSOR_COUNT; i++)
-        if (isBlack(sensorValues[i]))
-            k = 2;
-    if (k == 2)
-        return true;
-    return false;
+  return (isBlack(sensorValues[5]) && isBlack(sensorValues[6]) && isBlack(sensorValues[7]));
 }
-
-bool allBlack()
-{
-    for (int i = 0; i < SENSOR_COUNT; i++)
-        if (!isBlack(sensorValues[i]))
-            return false;
-    return true;
-}
-
 bool allWhite()
 {
-    for (int i = 0; i < SENSOR_COUNT; i++)
-        if (isBlack(sensorValues[i]))
-            return false;
-    return true;
+  return (!middleLine && !lineLeft() && !lineRight());
+}
+bool allBlack()
+{
+  return (middleLine && lineLeft() && lineRight());
 }
 
-void lineFollow1()
+//GRIPPER
+void gripperServo(int pulse)
 {
-    if (allBlack())
+    for(int i = 0; i < GRIPPER_PULSE_REPEAT;i++)
     {
-        idle();
-        moveForward(200, 200);
-        delay(200);
-        idle();
+        digitalWrite(GRIPPER,HIGH);
+        delayMicroseconds(pulse);
+        digitalWrite(GRIPPER,LOW);
+        delay(20);
     }
-    else if (allWhite())
+}
+void openGripper()
+{
+    gripperServo(GRIPPER_OPEN_PULSE);
+}
+void closeGripper()
+{
+  gripperServo(GRIPPER_CLOSE_PULSE);
+}
+
+//LINE MAZE
+void beginRace()
+{
+  delay(2000);
+  int lines=0;
+  forward(MIN_L,MIN_R);
+  while (true)
+  {
+    lineSensors.read(sensorValues);
+    if (lines>4)
     {
-        moveForward(200, 0);
+      stop();
+      delay(200);
+      openGripper();
+      delay(50);
+      closeGripper();
+      delay(1000);
+      leftTurn(28);
+      break;
     }
-    else if (lineAhead())
+    else if (allBlack())
     {
-        moveForward(200, 200);
+      lines++;
+      delay(200);
     }
-    else if (lineRight())
+  }
+  set=true;
+}
+void maze()
+{
+  if (lineRight() && isBlack(sensorValues[4]))
+  {
+    forward();
+    delay(100);
+    stop();
+    while (true)
     {
-        idle();
-        moveForward(200, 0);
+      lineSensors.read(sensorValues);
+      if (allBlack())
+      {
+        stop();
+        delay(250);
+        openGripper();
+        solved=true;
+        break;
+      }
+      else
+      {
+        rightTurn(42);
+        break;
+      }
     }
-    else if (lineLeft())
+  }
+  else if (middleLine())
+  {
+    forward();
+  }
+  else if (lineRight())
+  {
+    adjustRight();
+  }
+  else if (lineLeft())
+  {
+    adjustLeft();
+  }
+  else if (allBlack())
+  {
+    forward();
+    delay(100);
+    leftTurn2(26);
+    while(true)
     {
-        idle();
-        moveForward(0, 200);
+      lineSensors.read(sensorValues);
+      if (allBlack())
+      {
+        break;
+      }
+      if (allWhite())
+      {
+        leftSpin();
+      }
     }
-    else if (mostlyBlack())
-    {
-        idle();
-        moveForward(200, 200);
-        delay(200);
-        idle();
-    }
-    else
-        idle();
+  }
+  
+}
+void endRace()
+{
+  leftTurn(200);
+  stop();
+  rightTurn(200);
+  stop();
 }
